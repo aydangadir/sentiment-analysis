d31e4fea3e6f32772ad1c3a508ba371195fd3f59
Aydan Gadirzade <aydangadirzade1@gmail.com>
2023-10-21 22:13:58 +0400

    added textblob method

---
+++ models/textblob_.py
@@ -0,0 +1,46 @@
+from textblob import TextBlob
+
+class textblob_:
+    def __init__(self, text: str):
+        """
+        Initializes the textblob_ class with the provided text.
+
+        Args:
+            text (str): The text for sentiment analysis.
+        """
+        self.text = text
+        self.blob = TextBlob(text)
+    
+    def get_polarity(self) -> float:
+        """
+        Calculates the polarity of the text.
+
+        Returns:
+            float: The polarity score of the text.
+        """
+        return self.blob.sentiment.polarity
+
+    def get_sentiment(self) -> str:
+        """
+        Determines the sentiment of the text based on its polarity.
+
+        Returns:
+            str: The sentiment label ('Negative', 'Neutral', or 'Positive').
+        """
+        score = self.get_polarity()
+
+        if score < 0:
+            return "Negative"
+        elif score == 0:
+            return "Neutral"
+        else:
+            return "Positive"
+    
+    def get_subjectivity(self) -> float:
+        """
+        Calculates the subjectivity of the text.
+
+        Returns:
+            float: The subjectivity score of the text ranging [0:1] -> 0: fact, 1: public opinion
+        """
+        return self.blob.sentiment.subjectivity
